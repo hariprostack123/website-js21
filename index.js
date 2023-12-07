e3765866175f24c530ba68d4e53c2cc2981376be
hariprostack123 <128031941+hariprostack123@users.noreply.github.com>
2023-12-07 12:54:36 +0530

    js21

---
+++ index.js
@@ -0,0 +1,12 @@
+ // for loop = repeat some code a
+ //            certain amount of times
+
+/*for(let counter = 50; counter <= 100; counter+=1){
+    console.log(counter);
+}
+*/
+for(let i = 10; i > 0; i-=3){
+    console.log(i);
+}
+
+console.log("HAPPY NEW YEAR!");
